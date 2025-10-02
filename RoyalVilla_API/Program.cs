da5938bda4e31b9e3e09e115944faa5be0bce134
bhrugen <bhrugen1990@gmail.com>
2025-10-02 10:48:58 -0500

    Section 2 - Add Scalar

--- RoyalVilla_API/Program.cs
+++ RoyalVilla_API/Program.cs
@@ -1,4 +1,4 @@
-
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
