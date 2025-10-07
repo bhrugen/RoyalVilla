765e4093ff2b748bfdf4b5eb08ccffbac38169f6
bhrugen <bhrugen1990@gmail.com>
2025-10-07 14:44:59 -0500

    Section 9 - Villa List Part 1

---
+++ RoyalVillaWeb/Controllers/VillaController.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services;
+using RoyalVillaWeb.Services.IServices;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper )
+        {
+            _mapper = mapper;
+            _villaService = villaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaList = new();
+            try
+            {
+                var response = await _villaService.GetAllAsync<ApiResponse<List<VillaDTO>>>("");
+                if(response!=null && response.Success && response.Data != null)
+                {
+                    villaList= response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred: {ex.Message}";
+            }
+
+            return View(villaList);
+        }
+
+    }
+}
