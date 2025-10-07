39f7203e59d3d7229f9054504d9db68b5b0a926d
bhrugen <bhrugen1990@gmail.com>
2025-10-06 19:55:21 -0500

    Section 8 - Villa Service in Home Controller

--- RoyalVillaWeb/Controllers/HomeController.cs
+++ RoyalVillaWeb/Controllers/HomeController.cs
@@ -1,14 +1,42 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
 using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services;
+using RoyalVillaWeb.Services.IServices;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace RoyalVillaWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public HomeController(IVillaService villaService, IMapper mapper )
         {
-            return View();
+            _mapper = mapper;
+            _villaService = villaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaList = new();
+            try
+            {
+                var response = await _villaService.GetAllAsync<ApiResponse<List<VillaDTO>>>("");
+                if(response!=null && response.Success && response.Data != null)
+                {
+                    villaList= response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred: {ex.Message}";
+            }
+
+            return View(villaList);
         }
 
         public IActionResult Privacy()
