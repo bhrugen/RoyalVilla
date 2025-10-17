02113064f9abea9ea0632a586ef4cdeb6fa34627
bhrugen <bhrugen1990@gmail.com>
2025-10-17 15:07:33 -0500

    Section 10 - Auth Controller

---
+++ RoyalVillaWeb/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+using System.Diagnostics;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly IMapper _mapper;
+
+        public AuthController(IAuthService authService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+            try
+            {
+                var response = await _authService.LoginAsync<ApiResponse<LoginResponseDTO>>(loginRequestDTO);
+                if (response != null && response.Success && response.Data != null)
+                {
+                    LoginResponseDTO model = response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred: {ex.Message}";
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterationRequestDTO
+            {
+                Email = string.Empty,
+                Name = string.Empty,
+                Password = string.Empty,
+                Role = "Customer"
+            });
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+
+    }
+}
+
