a3d26c1871ed5e1e069dbe7aa264dab11026eea5
bhrugen <bhrugen1990@gmail.com>
2025-10-30 18:56:21 -0500

    Section 16 - Token Provider Interface

---
+++ RoyalVillaWeb/Services/IServices/ITokenProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace RoyalVillaWeb.Services.IServices
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+        string? GetToken();
+        void ClearToken();
+        ClaimsPrincipal? GetClaimsFromToken(string token);
+    }
+}
---
+++ RoyalVillaWeb/Services/TokenProvider.cs
@@ -0,0 +1,28 @@
+﻿using RoyalVillaWeb.Services.IServices;
+using System.Security.Claims;
+
+namespace RoyalVillaWeb.Services
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal? GetClaimsFromToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
